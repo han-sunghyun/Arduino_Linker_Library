3656e789c7c7e07dd42718079d0810194cc395f4
sunghyun <sunghyun5322@gmail.com>
2026-05-28 16:30:20 +0900

    String dynamic memory is static memory change

--- src/SliderBulider.h
+++ src/SliderBulider.h
@@ -5,72 +5,76 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  */
- 
+
 class SliderStyleBuilder {
 private:
-  int _id = 0;
-  int _value = 0;
-  String _text = "";
-  String _textColor = "";
-  String _sliderHaneColor = "";
-  String _sliderLineColor = "";
+    int _id = 0;
+    int _value = 0;
+    char _text[21] = {0};
+    char _textColor[7] = {0};
+    char _sliderHandColor[7] = {0}; // 오타 수정 (Hane -> Hand)
+    char _sliderLineColor[7] = {0};
 
 public:
-  SliderStyleBuilder& setId(int id) {
-    _id = id;
-    return *this;
-  }
+    SliderStyleBuilder& setId(int id) {
+        _id = id;
+        return *this;
+    }
 
-  SliderStyleBuilder& setValue(int val) {
-    _value = val;
-    return *this;
-  }
+    SliderStyleBuilder& setValue(int val) {
+        _value = val;
+        return *this;
+    }
 
-  SliderStyleBuilder& setText(int val) {
-    _text = String(val);
-    return *this;
-  }
+    // 정수형 처리
+    SliderStyleBuilder& setText(int val) {
+        snprintf(_text, sizeof(_text), "%d", val);
+        return *this;
+    }
 
-  SliderStyleBuilder& setText(float val) {
-    _text = String(val);
-    return *this;
-  }
+    // 실수형 처리 (dtostrf 사용)
+    SliderStyleBuilder& setText(float val, int precision = 2) {
+        dtostrf(val, 1, precision, _text);
+        return *this;
+    }
 
-  SliderStyleBuilder& setText(char val) {
-    _text = String(val);
-    return *this;
-  }
+    // 단일 문자 처리
+    SliderStyleBuilder& setText(char val) {
+        _text[0] = val;
+        _text[1] = '\0';
+        return *this;
+    }
 
-  SliderStyleBuilder& setText(const String& val) {
-    _text = val;
-    return *this;
-  }
+    // 문자열(char 배열) 처리
+    SliderStyleBuilder& setText(const char* val) {
+        strncpy(_text, val, sizeof(_text) - 1);
+        _text[sizeof(_text) - 1] = '\0';
+        return *this;
+    }
 
-  SliderStyleBuilder& setTextColor(String color) {
-    _textColor = color;
-    return *this;
-  }
+    SliderStyleBuilder& setTextColor(const char* color) {
+        strncpy(_textColor, color, sizeof(_textColor) - 1);
+        _textColor[sizeof(_textColor) - 1] = '\0';
+        return *this;
+    }
 
-  SliderStyleBuilder& setSliderHandColor(String color) {
-    _sliderHaneColor = color;
-    return *this;
-  }
+    SliderStyleBuilder& setSliderHandColor(const char* color) {
+        strncpy(_sliderHandColor, color, sizeof(_sliderHandColor) - 1);
+        _sliderHandColor[sizeof(_sliderHandColor) - 1] = '\0';
+        return *this;
+    }
 
-  SliderStyleBuilder& setSliderLineColor(String color) {
-    _sliderLineColor = color;
-    return *this;
-  }
+    SliderStyleBuilder& setSliderLineColor(const char* color) {
+        strncpy(_sliderLineColor, color, sizeof(_sliderLineColor) - 1);
+        _sliderLineColor[sizeof(_sliderLineColor) - 1] = '\0';
+        return *this;
+    }
 
-  String build() {
-    String result = "SL:";
-    result += String(_id) + ",";
-    result += String(_value) + ",";
-    result += _text + ",";
-    result += _textColor + ",";
-    result += _sliderHaneColor + ",";
-    result += _sliderLineColor + "\n";
-    return result;
-  }
+    // 결과물을 외부 버퍼에 담아주는 안전한 build 함수
+    void build(char* outBuffer, size_t bufferSize) {
+        snprintf(outBuffer, bufferSize, "SL:%d,%d,%s,%s,%s,%s\n", 
+                 _id, _value, _text, _textColor, _sliderHandColor, _sliderLineColor);
+    }
 };
--- src/SwitchBuilder.h
+++ src/SwitchBuilder.h
@@ -5,69 +5,77 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  */
- 
+
 class SwitchStyleBuilder {
 private:
-  int _id = 0;
-  int _value = -1;
-  String _text = "";
-  String _textColor = "";
-  String _switchColor = "";
+    int _id = 0;
+    int _value = -1;
+    char _text[21] = {0};
+    char _textColor[7] = {0};
+    char _switchColor[7] = {0};
 
 public:
-  SwitchStyleBuilder& setId(int id) {
-    _id = id;
-    return *this;
-  }
-
-  SwitchStyleBuilder& setValue(bool val) {
-    _value = val;
-    return *this;
-  }
-
-  SwitchStyleBuilder& setText(int val) {
-    _text = String(val);
-    return *this;
-  }
-
-  SwitchStyleBuilder& setText(float val) {
-    _text = String(val);
-    return *this;
-  }
-
-  SwitchStyleBuilder& setText(char val) {
-    _text = String(val);
-    return *this;
-  }
-
-  SwitchStyleBuilder& setText(const String& val) {
-    _text = val;
-    return *this;
-  }
-
-  SwitchStyleBuilder& setTextColor(String color) {
-    _textColor = color;
-    return *this;
-  }
-
-  SwitchStyleBuilder& setSwitchColor(String color) {
-    _switchColor = color;
-    return *this;
-  }
-
-  String build() {
-    String result = "SW:";
-    result += String(_id) + ",";
-    if(_value == -1) {
-      result += ",";
-    } else {
-      result += String(_value) + ",";
+    SwitchStyleBuilder& setId(int id) {
+        _id = id;
+        return *this;
     }
-    result += _text + ",";
-    result += _textColor + ",";
-    result += _switchColor + "\n";
-    return result;
-  }
+
+    SwitchStyleBuilder& setValue(bool val) {
+        _value = val ? 1 : 0; // bool 값을 0 또는 1로 명확히 저장
+        return *this;
+    }
+
+    // 정수형 처리
+    SwitchStyleBuilder& setText(int val) {
+        snprintf(_text, sizeof(_text), "%d", val);
+        return *this;
+    }
+
+    // 실수형 처리 (dtostrf 사용)
+    SwitchStyleBuilder& setText(float val, int precision = 2) {
+        dtostrf(val, 1, precision, _text);
+        return *this;
+    }
+
+    // 단일 문자 처리
+    SwitchStyleBuilder& setText(char val) {
+        _text[0] = val;
+        _text[1] = '\0';
+        return *this;
+    }
+
+    // 문자열(char 배열) 처리
+    SwitchStyleBuilder& setText(const char* val) {
+        strncpy(_text, val, sizeof(_text) - 1);
+        _text[sizeof(_text) - 1] = '\0';
+        return *this;
+    }
+
+    SwitchStyleBuilder& setTextColor(const char* color) {
+        strncpy(_textColor, color, sizeof(_textColor) - 1);
+        _textColor[sizeof(_textColor) - 1] = '\0';
+        return *this;
+    }
+
+    SwitchStyleBuilder& setSwitchColor(const char* color) {
+        strncpy(_switchColor, color, sizeof(_switchColor) - 1);
+        _switchColor[sizeof(_switchColor) - 1] = '\0';
+        return *this;
+    }
+
+    // 결과물을 외부 버퍼에 담아주는 안전한 build 함수
+    void build(char* outBuffer, size_t bufferSize) {
+        char valBuffer[4] = {0};
+        
+        // _value가 -1이면 빈 문자열, 0이나 1이면 해당 숫자를 임시 버퍼에 생성
+        if (_value != -1) {
+            snprintf(valBuffer, sizeof(valBuffer), "%d", _value);
+        }
+
+        // 최종 조립 (valBuffer가 비어있으면 SW:1,,text... 형태로 자연스럽게 콤마만 찍힙니다)
+        snprintf(outBuffer, bufferSize, "SW:%d,%s,%s,%s,%s\n", 
+                 _id, valBuffer, _text, _textColor, _switchColor);
+    }
 };
